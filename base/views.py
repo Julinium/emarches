676e66a1dd7887693490471347e0ea33f1fe45ed
Julinium <julinium@gmail.com>
2025-10-08 01:36:49 +0100

    testing

--- base/views.py
+++ base/views.py
@@ -1,16 +1,5 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-# from base.models import Client
 
 def home(request):
-    # clients = Client.objects.filter(short__contains='/')
-
-    # i = 0
-    # for c in clients:
-    #     c.short = c.short.replace("/", "").strip()
-    #     c.save()
-    #     i += 1
-
-
-    # return HttpResponse(f"============ {i} / { clients.count() } ============")
-    return HttpResponse(f"========================")
+    return HttpResponse(f"============ Fine ============")
