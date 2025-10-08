6561464331860958e6a3c3e45a3ed74803420134
Julinium <julinium@gmail.com>
2025-10-08 11:35:34 +0100

    cleaning

--- emarches/urls.py
+++ emarches/urls.py
@@ -1,10 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
@@ -23,12 +16,8 @@ urlpatterns = []
 
 urlpatterns += i18n_patterns(
     path('', include('base.urls')),
-    # path('crm/', include('crm.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    # path('cons/', include('portal.urls')),
 )
 
-# TODO: In production, serve static files using nginx
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
