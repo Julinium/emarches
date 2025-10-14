d023f2071eb49946a4fdde2c0bb3c6a31f16e7a5
Julinium <julinium@gmail.com>
2025-10-14 18:20:29 +0100

    updated Models

--- nas/models.py
+++ nas/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.templatetags.static import static
+from django.utils.translation import gettext_lazy as _
 
 from base.models import Agrement, Tender, Qualif
 
@@ -54,7 +55,7 @@ class Company(models.Model):
     state     = models.CharField(max_length=64, blank=True, verbose_name=_('Region, State'))
     country   = models.CharField(max_length=64, blank=True, default=_('Morocco'), verbose_name=_('Country'))
     phone     = models.CharField(max_length=255, blank=True, verbose_name=_('Phone'))
-    email     = models.CharField(max_length=255, blank=True, verbose_name=_('Email'))
+    email     = models.EmailField(blank=True, verbose_name=_('Email'))
     whatsapp  = models.CharField(max_length=255, blank=True, verbose_name=_('Whatsapp'))
     faximili  = models.CharField(max_length=255, blank=True, verbose_name=_('Fax'))
     website   = models.CharField(max_length=128, blank=True, verbose_name=_('Website'))
@@ -87,13 +88,13 @@ class Company(models.Model):
 class Favorite(models.Model):
     id      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites', editable=False)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='favorites')
-    tender  = models.ForeignKey(Tender, on_delete=models.CASCADE, related_name='favorites')
-    folders = models.ManyToManyField(Folder, on_delete=models.DO_NOTHING, related_name='favorites')
-    active  = models.BooleanField(null=True, default=True)
-    when    = models.DateTimeField(blank=True, null=True)
-    tags    = models.CharField(max_length=128, blank=True, null=True)
-    comment = models.TextField(blank=True, null=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='favorites', verbose_name=_('Company'))
+    tender  = models.ForeignKey(Tender, on_delete=models.CASCADE, related_name='favorites', editable=False, verbose_name=_('Tender'))
+    folders = models.ManyToManyField(Folder, on_delete=models.DO_NOTHING, related_name='favorites', verbose_name=_('Folders'))
+    active  = models.BooleanField(null=True, default=True, editable=False)
+    when    = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Date Added'))
+    tags    = models.CharField(max_length=128, blank=True, null=True, verbose_name=_('Tags'))
+    comment = models.TextField(blank=True, null=True, verbose_name=_('Comment'))
 
     class Meta:
         db_table = 'nas_favorite'
@@ -104,11 +105,11 @@ class Favorite(models.Model):
 
 class Folder(models.Model):
     id      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folders')
-    name    = models.CharField(max_length=64, blank=True, null=True)
-    image   = models.ImageField(upload_to='folders/', null=True, blank=True)
-    color   = models.CharField(max_length=8, blank=True, null=True, default='#fd7e14')
-    comment = models.TextField(blank=True, null=True)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folders', editable=False)
+    name    = models.CharField(max_length=64, blank=True, null=True, verbose_name=_('Name'))
+    image   = models.ImageField(upload_to='folders/', null=True, blank=True, verbose_name=_('Image'))
+    color   = models.CharField(max_length=8, blank=True, null=True, default='#fd7e14', verbose_name=_('Color'))
+    comment = models.TextField(blank=True, null=True, verbose_name=_('Comment'))
 
     class Meta:
         db_table = 'nas_folder'
@@ -128,11 +129,11 @@ class Folder(models.Model):
 
 class Download(models.Model):
     id         = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user       = models.ForeignKey(User, on_delete=models.CASCADE, related_name='downloads')
-    tender     = models.ForeignKey(Tender, on_delete=models.CASCADE, related_name='downloads')
-    when       = models.DateTimeField(blank=True, null=True)
-    size_read  = models.CharField(max_length=128, blank=True, null=True)
-    size_bytes = models.BigIntegerField(blank=True, null=True)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE, related_name='downloads', editable=False)
+    tender     = models.ForeignKey(Tender, on_delete=models.CASCADE, related_name='downloads', editable=False, verbose_name=_('Tender'))
+    when       = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Date Started'))
+    size_read  = models.CharField(max_length=128, blank=True, null=True, editable=False, verbose_name=_('Displayed Size'))
+    size_bytes = models.BigIntegerField(blank=True, null=True, editable=False, verbose_name=_('Size in Bytes'))
 
     class Meta:
         db_table = 'nas_tender_download'
@@ -143,19 +144,20 @@ class Download(models.Model):
 
 
 class Letter(models.Model):
-    id      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user    = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='letters')
-    when    = models.DateTimeField(blank=True, null=True)
-    ua      = models.CharField(max_length=512, blank=True, null=True)
-    ip      = models.CharField(max_length=64, blank=True, null=True)
-    email   = models.CharField(max_length=128, blank=True, null=True)
-    phone   = models.CharField(max_length=64, blank=True, null=True)
-    title   = models.CharField(max_length=256, blank=True, null=True)
-    message = models.TextField(blank=True, null=True)
-    method  = models.CharField(max_length=8, blank=True, null=True)
-    comment = models.TextField(blank=True, null=True)
-    actions = models.TextField(blank=True, null=True)
-    solved  = models.BooleanField(blank=True, null=True, default=False)
+    id       = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user     = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='letters', editable=False)
+    when     = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Date sent'))
+    ua       = models.CharField(max_length=512, blank=True, null=True, editable=False, verbose_name=_('User Agent'))
+    ip       = models.CharField(max_length=64, blank=True, null=True, editable=False, verbose_name=_('IP Address'))
+    email    = models.EmailField(blank=True, null=True, editable=False, verbose_name=_('Email'))
+    phone    = models.CharField(max_length=64, blank=True, null=True, editable=False, verbose_name=_('Phone'))
+    title    = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Object'))
+    message  = models.TextField(blank=True, null=True, editable=False, verbose_name=_('Your Message'))
+    method   = models.CharField(max_length=8, blank=True, null=True, editable=False, verbose_name=_('Contact Method'))
+    response = models.TextField(blank=True, null=True, editable=False, verbose_name=_('Response'))
+    comment  = models.TextField(blank=True, null=True, verbose_name=_('Comment'))
+    actions  = models.TextField(blank=True, null=True, verbose_name=_('Actions'))
+    solved   = models.BooleanField(blank=True, null=True, default=False, verbose_name=_('Solved'))
 
     class Meta:
         db_table = 'nas_letter'
@@ -163,3 +165,130 @@ class Letter(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Newsletter(models.Model):
+    CHANNEL_CHOICES = [
+        ('email', _('Email')),
+        ('phone', _('Phone')),
+        ('sms', _('SMS')),
+        ('whatsapp', _('Whatsapp')),
+    ]
+
+    id          = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    active      = models.BooleanField(null=True, default=True)
+    name        = models.CharField(max_length=64, blank=True, null=True, verbose_name=_('Name'))
+    description = models.CharField(max_length=64, blank=True, null=True, verbose_name=_('Description'))
+    channel     = models.CharField(max_length=10, choices=CHANNEL_CHOICES, default='email', verbose_name=_('Channel'))
+    monthly     = models.SmallIntegerField(blank=True, null=True, default=4, verbose_name=_('Messages per month'))
+
+    @property
+    def frequency(self):
+        m = self.monthly
+        freq = m
+        unit = _('Message')
+        slot = _('Month')
+        if m >= 7 and m <= 30:
+            slot = _('Week')
+            freq = int(7 * m / 30)
+        elif m > 30:
+            slot = _('Day')
+            freq = int(m / 30)
+
+        return f'~{freq} {unit} / {slot}'
+
+    class Meta:
+        db_table = 'nas_newsletter'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+class Notification(models.Model):
+    CHANNEL_CHOICES = [
+        ('email', _('Email')),
+        ('phone', _('Phone')),
+        ('sms', _('SMS')),
+        ('whatsapp', _('Whatsapp')),
+    ]
+
+    id          = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    active      = models.BooleanField(null=True, default=True)
+    name        = models.CharField(max_length=64, blank=True, null=True, verbose_name=_('Name'))
+    event       = models.CharField(max_length=256, blank=True, null=True, verbose_name=_('Event'))
+    description = models.CharField(max_length=64, blank=True, null=True, verbose_name=_('Description'))
+    channel     = models.CharField(max_length=10, choices=CHANNEL_CHOICES, default='email', verbose_name=_('Channel'))
+
+    class Meta:
+        db_table = 'nas_notification'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class NewsletterSubscription(models.Model):
+    id         = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    active     = models.BooleanField(null=True, default=True)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='newsletters', editable=False)
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE, blank=True, null=True, related_name='subscriptions', editable=False)
+    when       = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Last subscribed'))
+
+    class Meta:
+        db_table = 'nas_newsletter_subscription'
+        ordering = ['-when']
+
+
+class NotificationSubscription(models.Model):
+    id           = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    active       = models.BooleanField(null=True, default=True)
+    user         = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='notifications', editable=False)
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE, blank=True, null=True, related_name='notifications', editable=False)
+    when         = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Last subscribed'))
+
+    class Meta:
+        db_table = 'nas_notification_subscription'
+        ordering = ['-when']
+
+
+class LetterSent(models.Model):
+    id           = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user         = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='sent_letters', editable=False, verbose_name=_('User'))
+    newsletter   = models.ForeignKey(Newsletter, on_delete=models.CASCADE, blank=True, null=True, related_name='sent_letters', editable=False, verbose_name=_('Newsletter'))
+    when         = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Date sent'))
+    sender       = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Sender'))
+    destination  = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Destination'))
+    title        = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Object'))
+    message      = models.TextField(blank=True, null=True, editable=False, verbose_name=_('Message'))
+    retries      = models.SmallIntegerField(default=0, verbose_name=_('Retries'))
+    max_retries  = models.SmallIntegerField(default=5, verbose_name=_('Max retries'))
+    success      = models.BooleanField(null=True, default=False, verbose_name=_('Success'))
+
+    class Meta:
+        db_table = 'nas_letter_sent'
+        ordering = ['success', '-when']
+
+    def __str__(self):
+        return f'{self.title}_{self.user}_{self.when}'
+
+
+class NotificationSent(models.Model):
+    id           = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user         = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='sent_notifications', editable=False, verbose_name=_('User'))
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE, blank=True, null=True, related_name='sent_notifications', editable=False, verbose_name=_('Notification'))
+    when         = models.DateTimeField(blank=True, null=True, auto_now_add=True, editable=False, verbose_name=_('Date sent'))
+    sender       = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Sender'))
+    destination  = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Destination'))
+    title        = models.CharField(max_length=256, blank=True, null=True, editable=False, verbose_name=_('Object'))
+    message      = models.TextField(blank=True, null=True, editable=False, verbose_name=_('Message'))
+    retries      = models.SmallIntegerField(default=0, verbose_name=_('Retries'))
+    max_retries  = models.SmallIntegerField(default=5, verbose_name=_('Max retries'))
+    success      = models.BooleanField(null=True, default=False, verbose_name=_('Success'))
+
+    class Meta:
+        db_table = 'nas_notification_sent'
+        ordering = ['success', '-when']
+
+    def __str__(self):
+        return f'{self.title}_{self.user}_{self.when}'
