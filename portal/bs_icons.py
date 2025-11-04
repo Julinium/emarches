4b0549ced59ac3c31f32c1abc497cbc8df0519f7
Julinium <julinium@gmail.com>
2025-11-04 16:49:49 +0100

    Commits

--- portal/bs_icons.py
+++ portal/bs_icons.py
@@ -2,7 +2,7 @@
 bicons = {
     'filters'       : 'bi bi-front text-warning',
     'estimate'      : 'bi bi-cash-coin',
-    'bond'          : 'bi bi-safe',            
+    'bond'          : 'bi bi-safe',    
                     # bookmark-check
                     # piggy-bank 
                     # credit-card
@@ -10,7 +10,8 @@ bicons = {
     'multi_lots'    : 'bi bi-ui-radios-grid',  
                     # 'grid'
                     # 'ui-checks-grid'
-    'location'      : 'bi bi-pin-map' ,        
+    'category'      : 'bi bi-grid',
+    'location'      : 'bi bi-pin-map',
                     # 'geo'
     'client'        : 'bi bi-briefcase',       
                     # 'house-door' # 'bank'
@@ -19,23 +20,28 @@ bicons = {
     'reference'     : 'bi bi-tag',
     'restricted'    : 'bi bi-intersect text-danger',       
                     # 'bell-slash-fill text-danger'
+    'reserved_ico'  : 'bi bi-sign-yield',
+    'variant_ico'   : 'bi bi-shuffle',
     'reserved'      : 'bi bi-sign-yield-fill text-primary',
     'variant'       : 'bi bi-shuffle text-success',
+
     'has_agrements' : 'bi bi-shield-fill-check text-primary',
     'has_qualifs'   : 'bi bi-mortarboard-fill text-primary',
     'has_samples'   : 'bi bi-palette2 text-primary',
     'has_visits'    : 'bi bi-person-walking text-primary',
     'has_meetings'  : 'bi bi-chevron-bar-contract text-primary',
-    'changes'       : 'bi bi-activity',        
+    'changes'       : 'bi bi-activity',
                     # 'pencil-square'
     'favorites'     : 'bi bi-heart',
     'downloads'     : 'bi bi-arrow-down-square',
                     # 'download'
     'comments'      : 'bi bi-chat-square-quote',
+    'ebid'          : 'bi bi-laptop',
     'ebid_req'      : 'bi bi-pc-display-horizontal text-primary',
     'ebid_opt'      : 'bi bi-pc-display-horizontal text-success',
     'ebid_na'       : 'bi bi-pc-display-horizontal text-danger',
                     # 'laptop'
+    'esign'         : 'bi bi-usb-drive', 
     'esign_req'     : 'bi bi-usb-drive text-primary', 
     'esign_opt'     : 'bi bi-usb-drive text-success', 
     'esign_na'      : 'bi bi-usb-drive text-secondary',       
@@ -54,5 +60,9 @@ bicons = {
     'backspace'     : 'bi bi-backspace',
     'filter'        : 'bi bi-funnel',
                     # binoculars
-
+    'days_to_go'    : 'bi bi-calendar-x',
+    'title'         : 'bi bi-card-heading',
+    'procedure'     : 'bi bi-book',
+    'awarding'      : 'bi bi-filter-left',
+    'domains'       : 'bi bi-layers',
 }
