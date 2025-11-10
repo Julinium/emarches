8593a2b578ccb37f491ecab70fe817be3459f823
Julinium <julinium@gmail.com>
2025-11-10 13:14:50 +0100

    Commit

--- base/context_processors.py
+++ base/context_processors.py
@@ -4,13 +4,7 @@ from nas.models import UserSetting, Favorite
 def portal_context(request):
 
     context = {}
-    user = request.user
-    if not user or not user.is_authenticated:
-        return context
-        
-    user_settings = UserSetting.objects.filter(user = request.user).first()
-    faved_ids = user.favorites.values_list('tender', flat=True)
-
+    
     bicons = {
         'filters'         : 'bi bi-front text-warning',
         'estimate'        : 'bi bi-cash-coin',
@@ -113,11 +107,17 @@ def portal_context(request):
                             # 
     }
 
+    context['bicons']        = bicons
 
+    user = request.user
+    if not user or not user.is_authenticated:
+        return context
+        
+    user_settings = UserSetting.objects.filter(user = request.user).first()
+    faved_ids = user.favorites.values_list('tender', flat=True)
 
     context['user_settings'] = user_settings
     context['faved_ids']     = faved_ids
-    context['bicons']        = bicons
 
     return context
 
