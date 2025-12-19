3194436984bcdc157c8bd6eb75adc31a2407c7e0
Julinium <julinium@gmail.com>
2025-12-19 22:29:09 +0100

    Commits

--- bdc/urls.py
+++ bdc/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('',                    views.bdc_list,                 name='bdc_bdc_list'),
     path('<uuid:pk>/',          views.bdc_details,              name='bdc_bdc_detail'),
 
-    path('<uuid:pk>/<str:fn>/', views.bdc_items_pdf,            name='bdc_articles_pdf'),
-    path('<uuid:pk>/<str:fn>/', views.bdc_items_csv,            name='bdc_articles_csv'),
+    path('<uuid:pk>/pdf/<str:fn>/', views.bdc_items_pdf,            name='bdc_articles_pdf'),
+    path('<uuid:pk>/csv/<str:fn>/', views.bdc_items_csv,            name='bdc_articles_csv'),
 
     path('loc/',                views.locations_list,           name='bdc_locations_list'),
     path('cli/',                views.client_list,              name='bdc_client_list'),
@@ -19,3 +19,4 @@ urlpatterns = [
     path('<uuid:pk>/fav/del/',  views.bdc_stickies_remove,      name='bdc_bdc_stickies_remove'),
     path('fav/del/',            views.bdc_stickies_remove_all,  name='bdc_bdc_stickies_remove_all'),
 ]
+
