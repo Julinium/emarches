3d5cc0c9368e165611281c12a5df1500885e89bf
Julinium <julinium@gmail.com>
2026-03-29 22:35:29 +0100

    Commits

--- scraper/constants.py
+++ scraper/constants.py
@@ -83,9 +83,9 @@ with open(creds_json) as g:
 # Items per page on browser. Higher means less pages to handle by Chromium, and then less time
 LINES_PER_PAGE = "500" 
 # Look for Tenders with Deadline up to PORTAL_DDL_PAST_DAYS days in the past.
-PORTAL_DDL_PAST_DAYS = 90 # 365 * 1 
+PORTAL_DDL_PAST_DAYS = 30 # 365 * 1 
 # Download Tenders DCE with Deadline up to PORTAL_DCE_PAST_DAYS days in the past.
-PORTAL_DCE_PAST_DAYS = 90
+PORTAL_DCE_PAST_DAYS = 30
 # Look for Tenders with Deadline up to PORTAL_DDL_FUTURE_DAYS days in the future.
 PORTAL_DDL_FUTURE_DAYS = 365 * 1 
 # Look for Tenders published up to PORTAL_PUB_PAST_DAYS days in the past.
