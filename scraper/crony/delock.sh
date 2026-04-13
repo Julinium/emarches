068336e2a8fd53a4d47fd558cf5ed3e591394512
Julinium <julinium@gmail.com>
2026-04-13 23:01:48 +0100

    Commit

--- scraper/crony/delock.sh
+++ scraper/crony/delock.sh
@@ -3,9 +3,11 @@
 _crony_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 _lock_file="$_crony_dir/.lock"
 
-if test -e "$_lock_file"; then
-    echo "Trying to remove Lock file."
+if test -e "$_lock_file"; then    
+    echo "Lock file found. Trying removal ..."
     rm -f -- $_lock_file
+    echo "Removed Lock file. No error reported."
+
 else
     echo "Lock file not found."
 fi
