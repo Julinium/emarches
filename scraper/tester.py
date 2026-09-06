e5e411db5782a7f0eef0b5f57842acdfc3230926
Julinium <julinium@gmail.com>
2026-09-06 20:38:49 +0100

    Commits

--- scraper/tester.py
+++ scraper/tester.py
@@ -10,9 +10,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emarches.settings')
 django.setup()
 
 def main():
-    from downer import getFileables
-    ft = getFileables(past_days=90)
-    print(f"Found {len(ft)} Tenders with no DCE and deadline older than 90 days.")
+    from base.models import Tender
+    t = Tender.objects.filter(chrono=1033775).first()
+    if t:
+        print(f"Found Tender. Value: {t.has_samples}")
+        t.save()
+        print(f"Saved Tender. Value: {t.has_samples}")
+    # from downer import getFileables
+    # ft = getFileables(past_days=90)
+    # print(f"Found {len(ft)} Tenders with no DCE and deadline older than 90 days.")
     pass
 
 
